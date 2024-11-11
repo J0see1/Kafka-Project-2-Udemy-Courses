3b0af86c0e329769f263a38f004a7fddcc68202f
J0see1 <marcelalvin11@gmail.com>
2024-11-12 03:52:01 +0700

    fix: fixing problem with consumer that read ',' as a new coloumn

--- kafka-producer/kafka-producer.py
+++ kafka-producer/kafka-producer.py
@@ -7,15 +7,21 @@ producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
 topic_name = 'udemy-courses'
 
+dataset_path = 'dataset/udemy_courses_dataset.csv'
+
+def quote_fields(row):
+    return [f'"{field}"' if ',' in str(field) else str(field) for field in row]
+
 with open('dataset/udemy_courses_dataset.csv', 'r', encoding='utf-8') as file:
     csv_reader = csv.reader(file)
-    headers = next(csv_reader) 
+    headers = next(csv_reader)  
 
     for row in csv_reader:
-        message = ','.join(row).encode('utf-8')  
+        quoted_row = quote_fields(row)  
+        message = ','.join(quoted_row).encode('utf-8')  
         producer.send(topic_name, message)
-        print(f'Sent: {message}')
+        print(f'Sent: {message}')    
         
-        time.sleep(5)
+        time.sleep(random.uniform(0.1, 1.0))
 
 producer.close()
